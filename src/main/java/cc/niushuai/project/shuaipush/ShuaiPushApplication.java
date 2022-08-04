f6bea3b4d8e1951c7a93a6171c1d3c38041fd501
niushuai <niushuai@nti56.com>
2022-08-04 17:34:43 +0800

    feat: 初始化提交

---
+++ src/main/java/cc/niushuai/project/shuaipush/ShuaiPushApplication.java
@@ -0,0 +1,16 @@
+package cc.niushuai.project.shuaipush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author niushuai
+ */
+@SpringBootApplication
+public class ShuaiPushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuaiPushApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cc/niushuai/project/shuaipush/ShuaiPushApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.niushuai.project.shuaipush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuaiPushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
