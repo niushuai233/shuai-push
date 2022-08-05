96b8aea0386f19fd67e55eeb5520cb0e68c9aab1
niushuai <niushuai@nti56.com>
2022-08-05 11:26:48 +0800

    feat: 定义服务配置

---
+++ src/main/java/cc/niushuai/project/shuaipush/common/config/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package cc.niushuai.project.shuaipush.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * web config
+ *
+ * @author niushuai
+ * @date 2022/8/4 17:51
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+}
