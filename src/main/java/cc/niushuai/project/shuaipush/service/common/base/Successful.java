4a92c6a0b7d240bf9ff276af99cbf4f929c48e36
niushuai233 <niushuai951101@gmail.com>
2022-09-01 17:33:39 +0800

    feat: 成功接口标识

---
+++ src/main/java/cc/niushuai/project/shuaipush/service/common/base/Successful.java
@@ -0,0 +1,17 @@
+package cc.niushuai.project.shuaipush.service.common.base;
+
+/**
+ * 是否成功
+ *
+ * @author niushuai
+ * @date 2022/9/1 17:13
+ */
+public interface Successful {
+    /**
+     * 是否成功
+     *
+     * @author niushuai
+     * @date: 2022/9/1 17:13
+     */
+    boolean isSuccess();
+}
