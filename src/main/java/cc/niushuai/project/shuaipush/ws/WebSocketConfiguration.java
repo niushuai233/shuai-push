371c4975c118b4d608723fdac032696fb069868d
niushuai233 <niushuai951101@gmail.com>
2024-06-21 17:16:47 +0800

    feat: log info

--- src/main/java/cc/niushuai/project/shuaipush/ws/WebSocketConfiguration.java
+++ src/main/java/cc/niushuai/project/shuaipush/ws/WebSocketConfiguration.java
@@ -1,9 +1,11 @@
 package cc.niushuai.project.shuaipush.ws;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+@Slf4j
 @Configuration
 public class WebSocketConfiguration {
 
@@ -13,6 +15,7 @@ public class WebSocketConfiguration {
         ServerEndpointExporter exporter = new ServerEndpointExporter();
         
         // 手动注册 WebSocket 端点
+        log.info("websocket register ExchangeChannel point");
         exporter.setAnnotatedEndpointClasses(ExchangeChannel.class);
         
         return exporter;
